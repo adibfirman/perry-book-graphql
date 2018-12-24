c0e8c3263c58a5ab603b286961734cf1bebb9c4a
Adib Firman <dib.firman@gmail.com>
2018-12-24 14:14:58 +0700

    setup client

---
+++ client/src/components/BookList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class BookList extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book Name</li>
+        </ul>
+      </div>
+    )
+  }
+
+}
+
+export default BookList
